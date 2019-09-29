f91665b89cf6ac7c14a59cbe784248346d64f18b
MrZ4King <55938191+MrZ4King@users.noreply.github.com>
2019-09-29 13:34:59 +0800

    Add files via upload

---
+++ mr.z4king.sh
@@ -0,0 +1,34 @@
+clear
+figlet AIDILAKBAR | lolcat
+echo "=====================================
+
+# Nama       : Mr.Z4King
+# WhatsApp   : 082259434451
+# GitHub     : https://github.com/MrZ4King
+# Instagram  : @aidilmhmmd09
+
+======================================" | lolcat
+
+sleep 2
+
+echo "[1] SpamCall
+[2] SpamSMS
+[3] Exit" | lolcat
+echo "[*] Pilih Nomor : " | lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+Clear
+pkg install git
+pkg install figlet
+git clone https://github.com/ClayHackerTeam/Prankers
+cd Prankers
+sh Prankers.sh
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+Clear
+figlet Mr.Z4King | lolcat
+fi
